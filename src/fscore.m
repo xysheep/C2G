e13042ff42f7ce3fe7bcdca6fec12999c2785f6f
Xingyu Yang <xysheep@outlook.com>
2017-09-21 00:01:41 -0400

    remove unnecessary files

--- src/fscore.m
+++
@@ -1,6 +0,0 @@
-function score = fscore(obs,target)
-tp = sum(obs & target);
-fp = sum(obs & ~target);
-fn = sum(~obs & target);
-score = 2*tp/(2*tp+fp+fn);
-end
--- src/plotdensity.m
+++
@@ -1,9 +0,0 @@
-function plotdensity(x,y,f)
-[~,~,rank] = unique(f);
-rank = rank/max(rank);
-cm = redbluecmap(200); % returns the current color map
-
-colorID = max(1, sum(rank > 0:1/length(cm(:,1)):1,2)); 
-
-myColor = cm(colorID, :); % returns your color
-scatter(x,y,10,myColor);
